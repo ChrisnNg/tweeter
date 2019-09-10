7094d95577853716306581219ddb1b9bf8daa07e
ChrisnNg <cristopherng@hotmail.com>
2019-09-10 22:44:32 +0000

    implement char counter. no color when invalid yet

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,15 @@
-console.log('char counter loaded!');
+$(document).ready(function() {
+  // --- our code goes here ---
+  const composeTweet = document.getElementById('composeTweet');
+
+  function callback() {
+    let isValid = true;
+    const counter = $(this).parent().children("span.counter");
+    count = $(this).val().length;
+    counter.text(140 - count);
+
+  }
+  
+  // Add listener
+  composeTweet.addEventListener('keydown', callback);
+});
